16f4846d1804d9fe07fc914fa9c9a4cf367aa77d
nirobsarker012 <nirobsarker012@gmail.com>
2024-11-14 22:29:01 +0600

    tailwind_box

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:"media", //or media or class.
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
